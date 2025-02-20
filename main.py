e6dccab3dc7b14094f719dd4fcf05a172346a846
jag911 <tschmitzer@gmail.com>
2025-02-20 16:17:00 -0600

    Ch2: Player L3: FPS : Added clock and ticket to set fps to 60hz

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    py_clock = pygame.time.Clock()
+    dt = 0
     while True:
         #catch a break!
         for event in pygame.event.get():
@@ -18,6 +20,7 @@ def main():
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = py_clock.tick(60)
 
 if __name__ == "__main__":
     main()
