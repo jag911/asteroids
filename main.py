b1628de8d6efff63cb0cbf10024ad2f03b8ab363
jag911 <tschmitzer@gmail.com>
2025-02-20 16:00:58 -0600

    CH2: Player L1: Modules - Add constants.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-#!/bin/python3 
+#!/Users/thomas/bootdev/asteroids/venv/bin/python 
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
 import pygame
-
+from constants import *
 
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
