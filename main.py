29a4d9ff3e521e133ba5e027cbe05e6298076461
jag911 <tschmitzer@gmail.com>
2025-02-20 16:12:14 -0600

    CH2: Player L2: Game Loop : Added pygame screen with an infinite(ish) loop

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        #catch a break!
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
